a8d73ff80ef2d155af753b3f372fc7cfd1be6887
AndGarcia21 <andgarcia2120@gmail.com>
2026-04-26 20:11:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function verModelo(tipo) {
+
+    if (tipo === "luminaria") {
+        window.location.href = "luminaria.html";
+    }
+
+    if (tipo === "caja") {
+        window.location.href = "caja.html";
+    }
+}
